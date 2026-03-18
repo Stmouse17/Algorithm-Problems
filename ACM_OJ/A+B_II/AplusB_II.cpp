a64c2f0dd455ec0ff81f98ec2e3576d7dd32cf09
Sintream_ouse <sintream@outlook.com>
2026-03-18 14:40:37 +0800

    Add files via upload

---
+++ ACM_OJ/A+B_II/AplusB_II.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    while (cin >> N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            cout << a + b << endl;
+        }
+    }
+    return 0;
+}
